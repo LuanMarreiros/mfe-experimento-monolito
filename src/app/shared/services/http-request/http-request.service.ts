a07e385c3d90866d8754306bb0eb3d097204bc93
LuanMarreiros <luan.marreiros@hotmail.com>
2025-05-25 06:22:32 -0300

    refactor: ajustando gateway

--- src/app/shared/services/http-request/http-request.service.ts
+++ src/app/shared/services/http-request/http-request.service.ts
@@ -4,7 +4,7 @@ import { inject, Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class HttpRequestService {
   private httpClient = inject(HttpClient);
-  private url = 'https://d2sfhirfmqk3ax.cloudfront.net/v1/apiExperimento';
+  private url = 'https://d2sfhirfmqk3ax.cloudfront.net/v1/confirmar';
 
   public get() {
     return this.httpClient.get(this.url, {
